cbb75f666ed287c7f8fb8e04fac5a458df477099
Saravana-Erode <saravanaraja@istardevelopers.in>
2023-03-02 14:58:33 +0530

    deleted file

--- lab_5,6/odd_even.py
+++
@@ -1,6 +0,0 @@
-num = int(input("enter the num: "))
-result = num % 2
-if result > 0:
-    print("ODD")
-else:
-    print("EVEN")
--- lab_5,6/positive_negative.py
+++
@@ -1,6 +0,0 @@
-num = int(input("enter the num: "))
-
-if num >= 0:
-    print("the number is positive")
-else:
-    print("the number is negative")

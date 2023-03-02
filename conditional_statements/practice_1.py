02d374a5041df5a88f038e6fa06dea2d5755995d
Saravana-Erode <saravanaraja@istardevelopers.in>
2023-03-02 18:43:45 +0530

    conditional_statements commit

---
+++ conditional_statements/practice_1.py
@@ -0,0 +1,15 @@
+marks = int(input("Enter the Marks: "))
+
+if marks >= 85:
+    print(f"Grade-A is the marks for {marks}")
+elif (marks < 84) and (marks >= 70):
+    print(f"Grade-B is the marks for {marks}")
+elif (marks < 69) and (marks >= 55):
+    print(f"Grade-C is the marks for {marks}")
+elif (marks < 54) and (marks >= 40):
+    print(f"Grade-D is the marks for {marks}")
+elif marks < 40:
+    print(f"Grade-E is the marks for {marks} and Need an Improvement")
+
+
+
